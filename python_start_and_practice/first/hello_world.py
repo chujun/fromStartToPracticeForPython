6e84e3b78e076e7d6abb1b880772c6b170312330
jun.chu <1571970439@qq.com>
2019-11-26 15:16:42 +0800

    hello_wolrd.py

--- python_start_and_practice/first/hello_world.py
+++ python_start_and_practice/first/hello_world.py
@@ -1,3 +1,12 @@
+# encoding: utf-8
 print("hello word")
 message = "hello world"
 print(message)
+message = 'hello world for you'
+print(message)
+
+# 字符串
+a = 'this is "a"'
+b = "this is 'b'"
+c = """this is "c' """
+print (a+","+b+","+c)
