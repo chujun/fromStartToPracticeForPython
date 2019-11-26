c1491be536ba29523b32bf34ae9ba85ba72c01ae
jun.chu <1571970439@qq.com>
2019-11-26 18:06:18 +0800

    create bicycles.py

---
+++ python_start_and_practice/first/third_chapter/bicycles.py
@@ -0,0 +1,10 @@
+# encoding=utf-8
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+print(bicycles[0]+","+bicycles[1])
+# 访问最后一个元素[-1],索引-2访问倒数第二个元素 java没有
+print(bicycles[-1]+","+bicycles[-2])
+# IndexError: list index out of range
+# print(bicycles[-5])
+# print(bicycles[4])
+
