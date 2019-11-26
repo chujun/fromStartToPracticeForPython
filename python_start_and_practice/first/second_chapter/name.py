144fe6b38ff519bc0ca8779979e3a5fc771c8fa3
jun.chu <1571970439@qq.com>
2019-11-26 16:26:51 +0800

    second chapter

---
+++ python_start_and_practice/first/second_chapter/name.py
@@ -0,0 +1,21 @@
+# encoding:utf-8
+name = "那些人heLLo WoRD"
+# 单词首字母大写，其他字母小写
+print(name.title())
+
+print(name.upper())
+
+print(name.lower())
+
+# 那些人Hello Word
+# 那些人HELLO WORD
+# 那些人hello word
+
+
+# 制表符
+print('Language\n\tPython\n\tC\n\tJava')
+
+word = '\t word \n'
+print(word.title().strip() + "1")
+print(word.lstrip() + "1")
+print(word.rstrip() + "1")
---
+++ python_start_and_practice/first/second_chapter/str.py
@@ -0,0 +1,11 @@
+# encoding:utf-8
+age = 23
+print("Happy"+str(age)+"rd BirthDay")
+
+
+print("Happy"+age+"rd BirthDay")
+# error message:
+# Traceback (most recent call last):
+#  File "/Users/chujun/PycharmProjects/ahs/python_start_and_practice/first/second_chapter/str.py", line 3, in <module>
+#    print("Happy"+age+"rd BirthDay")
+# TypeError: can only concatenate str (not "int") to str
