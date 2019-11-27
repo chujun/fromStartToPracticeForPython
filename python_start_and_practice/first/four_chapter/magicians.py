191f10d1695f7990a9f438a145f117d74b0cb273
jun.chu <1571970439@qq.com>
2019-11-27 16:12:41 +0800

    magicians.py create

---
+++ python_start_and_practice/first/four_chapter/magicians.py
@@ -0,0 +1,8 @@
+# encoding=utf-8
+magicians = ['jun', 'bin', 'qi', 'feng']
+for magician in magicians:
+    print('welcome ' + magician.title())
+    print(magician + ",that was a great trick")
+# 虽然不推荐，但人家确实活着
+print("game end")
+print("变量仍然活着:" + magician)
