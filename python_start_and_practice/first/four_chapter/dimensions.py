e0e4ae4024bb7dc4c4683b585d9bc7e68ae333fd
jun.chu <1571970439@qq.com>
2019-11-28 18:13:44 +0800

    dimensions.py add

---
+++ python_start_and_practice/first/four_chapter/dimensions.py
@@ -0,0 +1,14 @@
+# encoding=utf-8
+# 元组
+dimensions = (80, 200, 500)
+print(dimensions)
+print(dimensions[0])
+print(dimensions[1])
+
+# TypeError: 'tuple' object does not support item assignment
+# dimensions[0] = 40
+
+# 修改元组变量，可以给整个变量重新赋值
+dimensions = (800, 200, 500, 600)
+print(dimensions)
+
