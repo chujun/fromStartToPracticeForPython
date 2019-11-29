731519830be87a3ad504c5b68f52ceae8214b6af
jun.chu <1571970439@qq.com>
2019-11-29 11:15:19 +0800

    cars.py dev finished

--- python_start_and_practice/first/five_chapter/cars.py
+++ python_start_and_practice/first/five_chapter/cars.py
@@ -22,3 +22,36 @@ if age > 18 and not is_male:
     print("come female")
 if age < 18 or age > 60:
     print("you need to be protected")
+
+if age < 5:
+    print('your age is too little,' + str(age))
+elif age > 60:
+    print('your age is too old,' + str(age))
+else:
+    print('your age is suitable,' + str(age))
+
+age = 17
+if age < 5:
+    print('your age is too little,' + str(age))
+elif age < 18:
+    print('sorry,your age have not enough,' + str(age))
+elif age > 60:
+    print('your age is too old,' + str(age))
+else:
+    print('your age is suitable,' + str(age))
+
+if my_car in cars:
+    print('my car have exist' + my_car)
+if my_car not in cars:
+    print('my car not exist ' + my_car)
+
+empty_cars = cars[:]
+empty_cars.clear()
+# 确定列表是不是空的
+if empty_cars:
+    print('not empty:' + str(empty_cars))
+if not empty_cars:
+    print('empty:' + str(empty_cars))
+for empty_car in empty_cars:
+    print(empty_car)
+
