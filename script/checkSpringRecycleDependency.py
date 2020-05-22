ccd2bda612bb4b2c3ee5670947b2ea6692b4865a
jun.chu <1571970439@qq.com>
2020-05-22 15:40:18 +0800

    feat(checkspringrecycledependency.py): quick check spring recycle dependency error

---
+++ script/checkSpringRecycleDependency.py
@@ -0,0 +1,26 @@
+# coding:utf-8
+
+def get_file_content(file_name):
+    with open(file_name) as file:
+        content = file.read()
+        #print(content)
+        return content
+
+
+if __name__ == '__main__':
+    file_name = '/Users/chujun/PycharmProjects/ahs/script/data/error/dependency.log'
+    content = get_file_content(file_name)
+    beans = []
+    for string in content.split(" "):
+        if (string.startswith("'") and (string.endswith("'") or string.endswith("':"))):
+            beans.append(string.strip(":").lstrip("'").rstrip("'"))
+    print(len(beans))
+    print(beans)
+    index = 1
+    for bean_name in beans:
+        str = ''
+        for i in range(1, index):
+            str += ' '
+        str += '===>' + bean_name
+        print(str)
+        index += 1
